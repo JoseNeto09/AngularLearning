f84f75aa6beba001a706a0ee5da921c0cc268755
joselopes <joselopessobrinhoneto9@gmail.com>
2021-12-08 09:39:51 -0300

    criando metodo extrato para o historico de transferencia

--- src/app/extrato/extrato.component.ts
+++ src/app/extrato/extrato.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-extrato',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./extrato.component.scss']
 })
 export class ExtratoComponent implements OnInit {
+  @Input () transferencia: any;
+
 
   constructor() { }
 

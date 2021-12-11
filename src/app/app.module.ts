234ea07e7e8b6facb9774aed2df529575340ef39
joselopes <joselopessobrinhoneto9@gmail.com>
2021-12-11 02:39:53 -0300

    organizando tabela ngFor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,24 @@
 import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
-import { NgModule } from '@angular/core';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { ExtratoComponent } from './extrato/extrato.component';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+
+registerLocaleData(localePt, 'pt');
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NovaTransferenciaComponent,
-    ExtratoComponent
+  declarations: [AppComponent, NovaTransferenciaComponent, ExtratoComponent],
+  imports: [BrowserModule, FormsModule],
+  providers: [
+    {provide: LOCALE_ID, useValue: 'pt'},
+    {
+      provide: DEFAULT_CURRENCY_CODE,
+      useValue: 'BRL',
+    },
   ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    AppRoutingModule
-  ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
